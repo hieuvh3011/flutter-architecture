4b6649fced627d54dede9128f2f65c522cf0bd60
Phuc Tran <huyphuc0906@gmail.com>
2020-01-05 16:31:54 +0700

    Add localization demo

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/localization/flutter_localizations.dart';
+import 'package:flutter_architecture/localization/text_names.dart';
 import 'package:flutter_architecture/res/images/images.dart';
 import 'package:flutter_architecture/router/routes.dart';
 import 'package:flutter_architecture/theme/colors.dart';
+import 'package:flutter_architecture/theme/dimens.dart';
 import 'package:flutter_architecture/utils/ui_utils.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -35,15 +38,30 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     final logoSize = getScreenWidth(context) / 4;
+    FlutterLocalizations _localization = FlutterLocalizations.of(context);
+    String appName = _localization.getString(context, TextName.appName);
 
     return Scaffold(
       backgroundColor: AppColors.white,
       body: Center(
-        child: Image.asset(
-            Images.logo,
-            width: logoSize,
-            height: logoSize,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+                Images.logo,
+                width: logoSize,
+                height: logoSize,
+              ),
+            SizedBox(height: Dimens.screenPaddingVertical,),
+            Text(
+              appName,
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: Dimens.headerTextSize
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/theme/dimens.dart
+++ lib/theme/dimens.dart
@@ -6,4 +6,6 @@ class Dimens {
       getScreenWidth(context) / 5;
   static const double screenPaddingHorizontal = 16;
   static const double screenPaddingVertical = 16;
+
+  static const double headerTextSize = 30;
 }
